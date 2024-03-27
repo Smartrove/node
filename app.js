a01fedc4e9ac1ab66fff6951207c76ce31d4c3c7
smartrove <olaokunolalekan@gmail.com>
2024-03-28 00:06:08 +0100

    update on waiting list form

--- app.js
+++ app.js
@@ -9,7 +9,30 @@ const helmet = require("helmet");
 const morgan = require("morgan");
 const Routes = require("./src/routes/routes");
 
-app.use(cors());
+const allowedOrigins = [
+  "http://localhost:4000",
+  "https://node-j7z3.onrender.com",
+];
+
+var corsOptions = {
+  origin: function (origin, callback) {
+    log.info(`<<<<< Request Origin: ${origin} -->>>>>`);
+    // log.info(origin)
+    if (!origin && dev) {
+      // if (true) {
+      //for bypassing postman req with  no origin
+      return callback(null, true);
+    }
+    if (allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
 app.use(morgan("tiny"));
 
 app.use(helmet());
